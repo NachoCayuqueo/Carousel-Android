7d32ec142ad0af1fa15fec791964331e8dc39fd5
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2021-09-02 19:03:10 -0300

    button funcionality

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,34 +2,63 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import me.relex.circleindicator.CircleIndicator2
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
 import org.imaginativeworld.whynotimagecarousel.listener.CarouselListener
+import org.imaginativeworld.whynotimagecarousel.listener.CarouselOnScrollListener
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class MainActivity : AppCompatActivity() {
 
     private val list = mutableListOf<CarouselItem>()
+    private var itemCarousel: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val carousel: ImageCarousel = findViewById(R.id.carousel)
+        val indicator = findViewById<CircleIndicator2>(R.id.custom_indicator)
+        val button = findViewById<Button>(R.id.buttonID)
 
-        list.add(CarouselItem("https://astelus.com/wp-content/viajes/Lago-Moraine-Parque-Nacional-Banff-Alberta-Canada.jpg","image_1"))
-        list.add(CarouselItem("https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg","image_2"))
-        list.add(CarouselItem("https://www.nationalgeographic.com.es/medio/2018/02/27/playa-de-isuntza-lekeitio__1280x720.jpg","image_3"))
+        list.add(CarouselItem(R.drawable.coliseo_roma,"Coliseo Romano"))
+        list.add(CarouselItem(R.drawable.cristo_redentor,"Cristo Redentor"))
+        list.add(CarouselItem(R.drawable.gran_muralla_china,"Muralla China"))
+        list.add(CarouselItem(R.drawable.machu_picchu,"Machu Picchu"))
+        list.add(CarouselItem(R.drawable.petra,"Petra"))
+        list.add(CarouselItem(R.drawable.taj_mahal,"Taj Mahal"))
+        list.add(CarouselItem(R.drawable.piramide_de_chichen_itza,"Piramide de Chichen Itza"))
 
         carousel.carouselListener = object: CarouselListener{
             override fun onClick(position: Int, carouselItem: CarouselItem) {
-                Toast.makeText(this@MainActivity,"${carouselItem.caption}",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"CLICK EN: ${carouselItem.caption}",Toast.LENGTH_SHORT).show()
             }
 
             override fun onLongClick(position: Int, dataObject: CarouselItem) {
+                Toast.makeText(this@MainActivity,"CLICk PRONUNCIADO EN: ${dataObject.caption}",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        carousel.onScrollListener = object: CarouselOnScrollListener{
+            override fun onScrollStateChanged(
+                recyclerView: RecyclerView,
+                newState: Int,
+                position: Int,
+                carouselItem: CarouselItem?
+            ) {
+                itemCarousel = carouselItem?.caption
             }
         }
 
+        button.setOnClickListener {
+            println("CLICK CON: $itemCarousel")
+        }
+
         carousel.addData(list)
+        carousel.setIndicator(indicator)
     }
 }
